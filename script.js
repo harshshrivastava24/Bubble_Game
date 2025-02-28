b98480e1bac183885a61adf6f1b1d783fd2faf83
Harsh Shrivastava <harshshrivastava2403@gmail.com>
2025-02-28 21:20:52 +0530

    Making Timer

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
-var bubble = '';
+var time = 60;
+function timeRun() {
+    var timer = setInterval(function () {
+
+        if(time>0){
+            time--;
+            document.querySelector("#timeval").innerHTML = time;
+        }
+
+        else {
+            clearInterval(timer);
+        }
+        
+    }, 1000)
+}
+
+function hitval() {
+    var random_num = Math.floor(Math.random()*10);
+    document.querySelector("#hitvalue").textContent = random_num; 
+}
+
+function makeBubble() {
+    var bubble = '';
 
 for (var i = 1; i<153; i++) {
     var random_num = Math.floor(Math.random()*10);
@@ -7,3 +29,8 @@ for (var i = 1; i<153; i++) {
 }
 
 document.querySelector("#pbtm").innerHTML = bubble
+}
+
+makeBubble();
+timeRun();
+hitval();
