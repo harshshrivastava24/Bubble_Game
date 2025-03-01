541dc4dce083ceca19478787c58d4feeea3f62c2
Harsh Shrivastava <harshshrivastava2403@gmail.com>
2025-03-01 16:18:10 +0530

    Prefinal

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 var time = 60;
+var score = 0;
+var random_hit;
+
+function changeScore() {
+    document.querySelector("#score").textContent = score;
+}
+
 function timeRun() {
     var timer = setInterval(function () {
 
@@ -9,14 +16,15 @@ function timeRun() {
 
         else {
             clearInterval(timer);
+            document.querySelector("#pbtm").innerHTML = `<h2>Game Over!</h2>`
         }
         
     }, 1000)
 }
 
 function hitval() {
-    var random_num = Math.floor(Math.random()*10);
-    document.querySelector("#hitvalue").textContent = random_num; 
+    random_hit = Math.floor(Math.random()*10);
+    document.querySelector("#hitvalue").textContent = random_hit; 
 }
 
 function makeBubble() {
@@ -31,6 +39,18 @@ for (var i = 1; i<153; i++) {
 document.querySelector("#pbtm").innerHTML = bubble
 }
 
+document.querySelector("#pbtm").addEventListener("click", function(details) {
+    var selected = details.target.textContent;
+
+    if(selected == random_hit) {
+        score += 10;
+        changeScore();
+        makeBubble();
+        hitval();
+    }
+})
+
 makeBubble();
 timeRun();
 hitval();
+changeScore();
